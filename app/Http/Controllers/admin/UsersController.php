d255e5875809878cedb16999866933e6b82059c0
hasan3754 <hasan3754@diu.edu.bd>
2020-03-19 20:36:25 +0600

    part 4

--- app/Http/Controllers/admin/UsersController.php
+++ app/Http/Controllers/admin/UsersController.php
@@ -8,14 +8,15 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct ( ) {
+
+  $this->middleware('auth');
+
+    }
     public function index()
     {
-        //
+        $users=User::all();
+        return view('admin.users.index')->with('users','$users');
     }
 
     /**
