e6ec35d3d09a3b7ad9abbe1278e3a848588850bc
hasan3754 <hasan3754@diu.edu.bd>
2020-03-31 06:00:37 +0600

    part 7

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -6,8 +6,31 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-                    @foreach($users as $user)
-                    {{$user->name}}-{{$user->email}}
+                   
+                <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($users as $user)
+                      <tr>
+                        <th scope="row">1</th>
+                      <td>{{$user->name}}</td>
+                      <td>{{$user->email}}</td>
+                        <td>
+                        <a href="{{route('admin.users.edit',$user->id)}}"> <button type="button" class="btn btn-success">Success</button></a>
+                        <a href="{{route('admin.users.destroy',$user->id)}}">    <button type="button" class="btn btn-danger">Danger</button></a>  
+                        </td>
+                      </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+
                 <div class="card-body">
                 </div>
             </div>
